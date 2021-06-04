87f42cd1f8d55c94cda4a6218fd8c27db6d36eed
alevinicius <alevinicius106@gmail.com>
2021-06-04 19:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEconomica.Beneficio.Testes.Dominio/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaixaEconomica.Beneficio.Testes.Dominio
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
